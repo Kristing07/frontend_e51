af8bd1df300d1a63845501238fcdf8edf4810305
KristingT <tabladakristing@gmail.com>
2025-10-09 09:42:17 -0600

    Probando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
-import './App.css'
+import './App.css';
+import Titulo from './components/Titulo.jsx';
+import Mensaje from './components/Mensaje.jsx';
 
 const App = () => {
   return(
     <>
-    <h1>componente principal</h1>
+    <h1>componente principal </h1>
+    < Titulo/>
+    < Mensaje />
     </>
   );
 }
 
-export default App
+export default App;
